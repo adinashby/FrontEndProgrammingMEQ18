df34297cc9787ff1dab8500544552c2ee9376a2e
Adin Ashby <47254289+adinashby@users.noreply.github.com>
2022-08-08 14:40:33 -0400

    Added Script.js

---
+++ MyFirstWebProject/script.js
@@ -0,0 +1,74 @@
+var myVariable = 5;
+
+// This is an in-line comment.
+
+/*
+* This is is a 
+multi-line comment
+*/
+
+/**
+boolean
+string
+bigint
+number
+object
+symbol
+null
+undefined
+*/
+
+myVariable = 8;
+
+var myString = "Hello";
+
+console.log(myVariable);
+console.log(myString);
+
+var myNumber = "";
+
+var calculation = myNumber + 5;
+
+console.log(myNumber);
+console.log(calculation);
+
+console.log(typeof calculation);
+
+var myString = "Vanier";
+
+console.log(myString);
+
+let myInteger = 10;
+myInteger = 12;
+
+console.log(myInteger);
+
+const THE_COLLEGE = "Vanier College";
+
+console.log(THE_COLLEGE);
+
+myInteger++;
+console.log(myInteger);
+
+myInteger--;
+console.log(myInteger);
+
+let myCalc = 2.5 + 3.7;
+console.log(myCalc);
+
+console.log(Math.floor(myCalc));
+
+myCalc += 4.3;
+console.log(myCalc);
+
+let myQuote = 'Alan said,\n "Peter is coming"';
+
+console.log(myQuote);
+
+let myConcat = "This is " + " a tutorial";
+console.log(myConcat);
+
+myConcat += " on Javascript";
+console.log(myConcat);
+
+console.log(myConcat.length);
